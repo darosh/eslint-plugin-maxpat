4c2ccab17d3235a675845ca8a32273627b399c15
Jan Forst <ensonador@gmail.com>
2025-09-05 09:02:56 +0200

    reorg

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,7 @@
 import { defineConfig } from 'eslint/config'
 import pluginJs from '@eslint/js'
 import eslintPlugin from 'eslint-plugin-eslint-plugin'
+import n from 'eslint-plugin-n'
 
 export default defineConfig([
   {
@@ -11,10 +12,20 @@ export default defineConfig([
     extends: ['js/recommended'],
   },
   {
+    name: 'eslint-plugin-n',
+    plugins: {
+      'n': n,
+    },
+    extends: ['n/recommended'],
+  },
+  {
     name: 'eslint/eslint-plugin',
     plugins: {
       'eslint-plugin': eslintPlugin,
     },
     extends: ['eslint-plugin/recommended'],
+    rules: {
+      'n/no-unpublished-import': 'off'
+    }
   }
 ])
